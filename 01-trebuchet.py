f77fd7d2bac4448ab44b505a6fc88c4b4a84a67f
Gian <gdass8000@gmail.com>
2023-12-11 00:23:39 -0300

    day one - trebuchet!?

---
+++ 01-trebuchet.py
@@ -0,0 +1,18 @@
+with open('./01-input.txt', "r", encoding='utf-8') as file:
+    text = file.read()
+soma = 0
+for line in text.split('\n'):
+    primeiro: str = ""
+    ultimo: str = ""
+    current_index: int = 0
+    for index in range(len(line)):
+        if line[index].isdigit():
+            primeiro = line[index]
+            ultimo = line[index]
+            break
+        current_index = index
+    for i in range(current_index, len(line)):
+        if line[i].isdigit():
+            ultimo = line[i]
+    soma += int(primeiro + ultimo)
+print(soma)
